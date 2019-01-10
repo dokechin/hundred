4ee8f92d0868dcbc2a919998b01cb4bbbf50293b
dokechin <ttatsumi@ra2.so-net.ne.jp>
2019-01-11 07:34:08 +0900

    import to require

--- clova/index.js
+++ clova/index.js
@@ -1,4 +1,4 @@
-import arrayShuffle from 'array-shuffle'
+const arrayShuffle = require('array-shuffle')
 const uuid = require('uuid').v4
 const _ = require('lodash')
 const { DOMAIN, ExtensionId, DEBUG } = require('../config')
