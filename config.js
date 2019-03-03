734e7806f596c2c18694ad7d52cb6e51e14586d9
dokechin <ttatsumi@ra2.so-net.ne.jp>
2019-03-03 20:05:08 +0900

    commit

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   SERVER_PORT: process.env.SERVER_PORT || 3000,
   DOMAIN: 'https://dokechin-hundred.glitch.me/clova',
-  ExtensionId: 'com.dokechin.handred',
+  ExtensionId: 'com.dokechin.hundred',
   DEBUG: process.env.DEBUG || false,
 }
