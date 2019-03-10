27999188480d73fab28ee4d89f85c6faf31b3ac5
dokechin <ttatsumi@ra2.so-net.ne.jp>
2019-03-10 21:31:25 +0900

    commit

--- clova/index.js
+++ clova/index.js
@@ -145,27 +145,22 @@ class CEKRequest {
     console.log('launchRequest')
     var cached = cache.get(this.session.user.userId)
 
-    if (typeof cached === 'undefined' || cached.index == 0) {
-      cekResponse.appendSpeechText({
+    cekResponse.appendSpeechText({
         lang: 'ja',
         type: 'URL',
         value: `${DOMAIN}/koto_01.mp3`,
-      })
-      cekResponse.appendSpeechText("百人一首をはじむる。読みしあとに、次、または、もう一度といいたまえ。")
-      cekResponse.appendSpeechText({
-        lang: 'ja',
-        type: 'URL',
-        value: `${DOMAIN}/mute_01sec.mp3`,
-      })
-    }
+    })
+    cekResponse.appendSpeechText("百人一首をはじむる。読みしあとに、次、または、もう一度といいたまえ。")
+    cekResponse.appendSpeechText({
+      lang: 'ja',
+      type: 'URL',
+      value: `${DOMAIN}/mute_01sec.mp3`,
+    })
     if (typeof cached === 'undefined' ) {
       cached = { order : arrayShuffle([...Array(100).keys()]), index : 0};
       cekResponse.appendSpeechText(FUDA[cached.order[cached.index]][0])
       cekResponse.appendSpeechText(FUDA[cached.order[cached.index]][1])
     } else {
-      if (cached.index == 0) {
-        cekResponse.appendSpeechText(FUDA[cached.order[cached.index]][0])
-      }
       cekResponse.appendSpeechText(FUDA[cached.order[cached.index]][1])
     }
     cache.set(this.session.user.userId, cached)
